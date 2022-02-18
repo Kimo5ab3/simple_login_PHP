f0a97c24829bd20fb0b83da6c8b7bc78c9067a2d
Matteo Romani <97728034+Kimo5ab3@users.noreply.github.com>
2022-02-18 20:38:54 +0100

    Update index.php

--- simple_login_PHP/index.php
+++ simple_login_PHP/index.php
@@ -3,8 +3,8 @@
 <div class="container d-flex justify-content-center">
     <div class="col-lg-6 mt-5">
         <h1>Welcome in this website</h1>
-        <h4 class="mt-3">If you want enter in the restricted area, please logged in</h4>
-        <a class="btn btn-primary mt-5" href="db/login.php">Log in</a>
+        <h4 class="mt-3">If you want enter in the restricted area, please register</h4>
+        <a class="btn btn-primary mt-5" href="db/signup.php">Sign up</a>
     </div>
 </div>
 
