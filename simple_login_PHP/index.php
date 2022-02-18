e1fb8007b008139731ff5d72a0c61cca9c7e13dd
Matteo Romani <97728034+Kimo5ab3@users.noreply.github.com>
2022-02-18 20:14:55 +0100

    Update index.php

--- simple_login_PHP/index.php
+++ simple_login_PHP/index.php
@@ -1,11 +1,5 @@
 <?php include "sections/header.php"?>
 
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area riservata</title>
-</head>
-<body>
 <div class="container d-flex justify-content-center">
     <div class="col-lg-6 mt-5">
         <h1>Welcome in this website</h1>
